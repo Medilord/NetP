774546b766cb345460ac3c34dd480d17f5cbc0e8
Medilord <medilord@yandex.ru>
2026-06-19 14:01:02 +1000

    Практическое задание 3, 2 семестр

--- LL4/practicum_react/src/App.js
+++ LL4/practicum_react/src/App.js
@@ -1,11 +1,13 @@
 import './CSS/App.css';
 import buildings from './data';
 import Table from './components/Table';
+import Sum from './components/sum';
 
 function App() {
   return (
     <main className="app">
       <h3>Самые высокие здания и сооружения</h3>
+      {/*<Sum></Sum>*/}
       <Table
         data={buildings}
         amountRows={15}
